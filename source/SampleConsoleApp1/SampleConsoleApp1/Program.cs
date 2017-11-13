d3d2aa3ec92138d9f9d004e3986e9cdbc6230da4
Dino R. LaMana <dino.lamana@ancile.com>
2017-11-13 13:01:48 -0500

    Adding Source Code

---
+++ source/SampleConsoleApp1/SampleConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace SampleConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
